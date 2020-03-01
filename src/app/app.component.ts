8b21265afe23e19433298390f565fc1503b760d1
Tristan <ghoststeam217@gmail.com>
2020-03-01 14:12:48 +1300

    Installed the CoHost template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'lt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   public title = 'lyte-tech';
+
+  /**
+   * Initialize
+   */
+  public ngOnInit() {
+  }
+
+  /**
+   * Initialize cohost
+   */
+  public ngAfterViewInit() {
+    $.getScript('/assets/cohost/js/main.js');
+  }
 }
