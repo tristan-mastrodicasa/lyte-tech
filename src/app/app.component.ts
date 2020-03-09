99ea561de188f09b0639be6d4c09aa216a9b21b8
Tristan <ghoststeam217@gmail.com>
2020-03-09 21:33:26 +1300

    FIXED: main.js being reloaded every page transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router, NavigationEnd } from '@angular/router';
 
-declare var $: any;
+import { main } from 'assets/cohost/js/main.js';
 
 @Component({
   selector: 'lt-root',
@@ -13,11 +12,11 @@ export class AppComponent implements OnInit {
 
   constructor(public router: Router) {
 
-    // For every route change reload the main JS file that initializes cohost javascript //
+    // For every route change re-initialize the main JS file that initializes cohost javascript //
     this.router.events.subscribe((event) => {
 
       if (event instanceof NavigationEnd) {
-        $.getScript('/assets/cohost/js/main.js');
+        main();
       }
 
     });
