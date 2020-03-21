05684c06d2640f213888208db7b3ef4c58da7e67
Tristan <ghoststeam217@gmail.com>
2020-03-21 15:27:07 +1300

    Added page transition animations for page loads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { trigger, transition, style, query, animate, state } from '@angular/animations';
 
 import { main } from 'assets/cohost/js/main';
 
@@ -7,21 +8,38 @@ import { main } from 'assets/cohost/js/main';
   selector: 'lt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routerAnimations', [
+      state('start', style({ opacity: 0.3 })),
+      state('end', style({ opacity: 1 })),
+      transition('end => start', [
+        query(':self', [
+          animate('200ms ease-out', style({ opacity: 0.3 })),
+        ]),
+      ]),
+      transition('start => end', [
+        query(':self', [
+          animate('200ms', style({ opacity: 1 })),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+
+  public routerState: 'start' | 'end' = 'end';
 
   constructor(public router: Router) {
+
     // For every route change re-initialize the main JS file that initializes cohost javascript //
     this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) this.routerState = 'start';
       if (event instanceof NavigationEnd) {
+        this.routerState = 'end';
         main();
       }
     });
-  }
 
-  /**
-   * Initialize
-   */
-  public ngOnInit() { }
+  }
 
 }
