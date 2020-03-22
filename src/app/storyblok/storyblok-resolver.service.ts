8c2c8b00de031ec17d9d072bf6a6a75f877d0df5
Tristan <ghoststeam217@gmail.com>
2020-03-22 16:10:16 +1300

    Added better support for published / draft when deploying

--- src/app/storyblok/storyblok-resolver.service.ts
+++ src/app/storyblok/storyblok-resolver.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 
 import { StoryblokService } from './storyblok.service';
+import { environment } from 'environments/environment';
 
 @Injectable()
 export class StoryblokResolverService implements Resolve<any> {
@@ -12,6 +13,6 @@ export class StoryblokResolverService implements Resolve<any> {
    * Get data from story block
    */
   public async resolve(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<any> {
-    return this.storyblokService.getStory(state.url.substring(1) || 'index', { version: 'draft' });
+    return this.storyblokService.getStory(state.url.substring(1) || 'index', { version: (environment.production ? 'published' : 'draft') });
   }
 }
