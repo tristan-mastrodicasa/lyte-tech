0f4a0b5f1fee8617a11502bf65ccd8d0b12440ec
Tristan <ghoststeam217@gmail.com>
2020-04-15 22:08:11 +1200

    View draft data in editor

--- src/app/storyblok/storyblok-resolver.service.ts
+++ src/app/storyblok/storyblok-resolver.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 
 import { StoryblokService } from './storyblok.service';
-import { environment } from 'environments/environment';
 
 @Injectable()
 export class StoryblokResolverService implements Resolve<any> {
@@ -13,6 +12,11 @@ export class StoryblokResolverService implements Resolve<any> {
    * Get data from story block
    */
   public async resolve(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<any> {
-    return this.storyblokService.getStory(state.url.substring(1) || 'index', { version: (environment.production ? 'published' : 'draft') });
+    let version = 'published';
+    const storyblok: any = window.storyblok;
+
+    if (storyblok.inIframe()) version = 'draft';
+
+    return this.storyblokService.getStory(state.url.substring(1) || 'index', { version });
   }
 }
