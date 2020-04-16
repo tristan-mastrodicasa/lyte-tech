d60058606742d3f98f8e61469730cf21bc5263e5
Tristan <ghoststeam217@gmail.com>
2020-04-16 23:39:48 +1200

    Dynamically enable firebase analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule, COLLECTION_ENABLED } from '@angular/fire/analytics';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from 'environments/environment';
+import { firebaseConfig } from './configs/firebase-config';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,16 @@ import { environment } from 'environments/environment';
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAnalyticsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
   ],
-  providers: [],
+  providers: [
+    { provide: COLLECTION_ENABLED, useValue: environment.production },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/configs/firebase-config.ts
@@ -0,0 +1,12 @@
+import { FirebaseOptions } from '@angular/fire';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyCcmE6aTjoFrnjp_zuMGc-tGQx37mPkpxs',
+  authDomain: 'lyte-tech.firebaseapp.com',
+  databaseURL: 'https://lyte-tech.firebaseio.com',
+  projectId: 'lyte-tech',
+  storageBucket: 'lyte-tech.appspot.com',
+  messagingSenderId: '804387615668',
+  appId: '1:804387615668:web:840da7b0e6c2741dbb78e1',
+  measurementId: 'G-EGG0PW5502',
+};
